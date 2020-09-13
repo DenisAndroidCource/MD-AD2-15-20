61c1a51aedf499ef65fde7c7bce8395940392baa
DenisLatushko <denis.it.cources@gmail.com>
2020-09-13 10:09:41 +0300

    Fragments

--- CourceExamples/fragmentexample/src/main/java/by/it/academy/fragmentexample/ImageGreenFragment.java
+++ CourceExamples/fragmentexample/src/main/java/by/it/academy/fragmentexample/ImageGreenFragment.java
@@ -8,15 +8,20 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 public class ImageGreenFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        Bundle bundle = getArguments();
-        bundle.getInt("KEY");
+//        Bundle bundle = getArguments();
+//        bundle.getInt("KEY");
 
         return inflater.inflate(R.layout.fragment_image_green, container, false);
+    }
+
+    private void addFragment() {
+        FragmentManager childFragmentManager = getChildFragmentManager();
     }
 }
